a0badaf14bff3a6e7604aca4b29dd3b7e4f90027
Pinderr1 <pinderdhami1@gmail.com>
2025-06-24 09:07:07 -0700

    Add new board games

--- games/index.js
+++ games/index.js
@@ -10,6 +10,8 @@ import GuessNumberClient, { Game as guessNumberGame, Board as GuessNumberBoard, 
 import BattleshipClient, { Game as battleshipGame, Board as BattleshipBoard, meta as battleshipMeta } from './battleship';
 import CheckersClient, { Game as checkersGame, Board as CheckersBoard, meta as checkersMeta } from './checkers';
 import DominoesClient, { Game as dominoesGame, Board as DominoesBoard, meta as dominoesMeta } from './dominoes';
+import DotsBoxesClient, { Game as dotsBoxesGame, Board as DotsBoxesBoard, meta as dotsBoxesMeta } from './dots-and-boxes';
+import SnakesLaddersClient, { Game as snakesLaddersGame, Board as SnakesLaddersBoard, meta as snakesLaddersMeta } from './snakes-and-ladders';
 
 export const games = {
   [ticTacToeMeta.id]: {
@@ -63,6 +65,18 @@ export const games = {
     Game: battleshipGame,
     Board: BattleshipBoard,
     meta: battleshipMeta,
+  },
+  [dotsBoxesMeta.id]: {
+    Client: DotsBoxesClient,
+    Game: dotsBoxesGame,
+    Board: DotsBoxesBoard,
+    meta: dotsBoxesMeta,
+  },
+  [snakesLaddersMeta.id]: {
+    Client: SnakesLaddersClient,
+    Game: snakesLaddersGame,
+    Board: SnakesLaddersBoard,
+    meta: snakesLaddersMeta,
   },
 };
 
