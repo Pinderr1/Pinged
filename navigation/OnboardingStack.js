4f61a014ebcb6da54aaa32e1224113481f6f634e
Pinderr1 <pinderdhami1@gmail.com>
2025-06-29 08:29:14 -0700

    Simplify onboarding navigation

--- navigation/OnboardingStack.js
+++ navigation/OnboardingStack.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OnboardingScreen from '../screens/OnboardingScreen';
-import AppStack from './AppStack';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,7 +9,6 @@ export default function OnboardingStack() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-      <Stack.Screen name="Main" component={AppStack} />
     </Stack.Navigator>
   );
 }
