a86237562837e10e991dbe403c99b6382549fd3a
Pinderr1 <pinderdhami1@gmail.com>
2025-07-09 15:45:37 -0700

    Enable deep linking

--- App.js
+++ App.js
@@ -16,6 +16,16 @@ import useRemoteConfig from './hooks/useRemoteConfig';
 import RootNavigator from './navigation/RootNavigator';
 import { useTheme } from './contexts/ThemeContext';
 
+const linking = {
+  prefixes: ['https://pinged.app', 'pinged://'],
+  config: {
+    screens: {
+      HomeScreen: 'home',
+      MatchScreen: 'match/:id',
+    },
+  },
+};
+
 const ThemedNotificationCenter = () => {
   const { theme } = useTheme();
   return <NotificationCenter color={theme.accent} />;
@@ -65,7 +75,7 @@ const AppInner = () => {
         keyboardVerticalOffset={60}
       >
         <ErrorBoundary>
-          <NavigationContainer>
+          <NavigationContainer linking={linking}>
             <RootNavigator />
             <DevBanner />
           </NavigationContainer>
