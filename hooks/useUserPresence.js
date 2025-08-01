4bfde715c51d29afc6176a18449dcd3f41a8ace3
Pinderr1 <pinderdhami1@gmail.com>
2025-08-01 00:22:15 -0700

    Use presence document instead of profile listener

---
+++ hooks/useUserPresence.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import firebase from '../firebase';
+
+export default function useUserPresence(uid) {
+  const [presence, setPresence] = useState(null);
+
+  useEffect(() => {
+    if (!uid) return undefined;
+    const unsub = firebase
+      .firestore()
+      .collection('presence')
+      .doc(uid)
+      .onSnapshot((doc) => {
+        setPresence(doc.data() || null);
+      });
+    return unsub;
+  }, [uid]);
+
+  return presence;
+}
